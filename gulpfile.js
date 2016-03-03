894fcbd6877780b8f393d87f0796e31a38622fd2
BigBlueHat <byoung@bigbluehat.com>
2016-03-03 13:26:53 -0500

    Now with more CouchApp!

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,15 @@ var
   build        = require('./semantic/tasks/build')
 ;
 
+var argv = require('yargs').argv;
 var browserify = require('browserify');
+var push = require('couchdb-push');
 var source = require('vinyl-source-stream');
+
+var couch_url = 'http://localhost:5984/clusers';
+if ('url' in argv) {
+  couch_url = argv.url;
+}
 
 /*******************************
              Tasks
@@ -24,12 +31,24 @@ gulp.task('default', function() {
   });
 
   gulp
-    .src(['src/index.html', 'semantic/dist/semantic.min.css'])
+    .src(['src/index.html', 'semantic/dist/**'])
     .pipe(gulp.dest('dist'));
 
   return b.bundle()
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('couchapp', function() {
+  gulp
+    .src(['dist/**'])
+    .pipe(gulp.dest('_design/clusers/_attachments'));
+
+  push(couch_url, '_design/clusers',
+    function(err, resp) {
+      if (err) throw JSON.stringify(err);
+      console.log(resp);
+    });
 });
 
 gulp.task('watch-ui', watch);
