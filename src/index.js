1b7e4ea0782962638809415fa84177cbaa43baeb
BigBlueHat <byoung@bigbluehat.com>
2016-03-03 13:30:45 -0500

    Make it work! Host it as a CouchApp, fill in form

--- src/index.js
+++ src/index.js
@@ -20,5 +20,31 @@ var cluser = new Vue({
     passwords_match: function() {
       return this.new_user.password === this.new_user.confirmpass;
     }
+  },
+  methods: {
+    makeUser: function() {
+      var self = this;
+      var db = new PouchDB(location.origin + '/_users', {
+        auth: {
+          username: self.admin.username,
+          password: self.admin.password
+        }
+      })
+
+      db.installUsersBehavior()
+        .then(function () {
+          return db.put({
+            _id: 'org.couchdb.user:' + self.new_user.username,
+            type: 'user',
+            name: self.new_user.username,
+            roles: [],
+            password: self.new_user.password
+          })
+        })
+        .then(function(resp) {
+          console.log('new user', resp);
+        })
+        .catch(console.log.bind(console));
+    }
   }
 });
