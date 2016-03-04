75d7a009e79aab6b3fdd94ad6484947e5180b74f
BigBlueHat <byoung@bigbluehat.com>
2016-03-04 09:34:33 -0500

    Switch to password_scheme: simple for Cloudant

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 var Vue = require('vue');
 var PouchDB = require('pouchdb');
-PouchDB.plugin(require('pouchdb-users'));
+
+var crypto = require('crypto');
+
+function generatePasswordHash(password) {
+  var salt = crypto.randomBytes(16).toString('hex');
+  var hash = crypto.createHash('sha1');
+  hash.update(password + salt);
+  return [hash.digest('hex'), salt];
+}
 
 var cluser = new Vue({
   el: '#user-maker',
@@ -31,20 +39,26 @@ var cluser = new Vue({
         }
       })
 
-      db.installUsersBehavior()
-        .then(function () {
-          return db.put({
+      db.get('org.couchdb.user:' + self.new_user.username)
+        .then(function(user) {
+          console.log('user exists', user);
+        })
+        .catch(function(err) {
+          var hashAndSalt = generatePasswordHash(self.new_user.password);
+          db.put({
             _id: 'org.couchdb.user:' + self.new_user.username,
-            type: 'user',
             name: self.new_user.username,
             roles: [],
-            password: self.new_user.password
+            password_sha: hashAndSalt[0],
+            salt: hashAndSalt[1],
+            password_scheme: 'simple',
+            type: 'user'
           })
-        })
-        .then(function(resp) {
-          console.log('new user', resp);
-        })
-        .catch(console.log.bind(console));
+          .then(function(resp) {
+            console.log('new user', resp);
+          })
+          .catch(console.log.bind(console));
+        });
     }
   }
 });
